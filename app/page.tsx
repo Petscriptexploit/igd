1fe933bc16b1b8db2605033197524887c72d4f27
Creambeam <andy.gallagher0714@gmail.com>
2025-12-15 14:13:40 -0600

    Add IdleGame component to Home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { IdleGame } from "@/components/idle-game"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-gray-950 to-gray-900 text-white">
+      <IdleGame />
+    </main>
+  )
+}
